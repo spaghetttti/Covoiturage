52e5aff87c561f4ab77f1a0ddd0881613ed1c3e9
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2023-10-02 12:24:40 +0200

    feat:db connection tested

--- index.php
+++ index.php
@@ -7,14 +7,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./style.css" />
-  <title>Covoiturage</title>
-</head>
-
+<?php include 'components/header.php'; ?>
 <body>
   <header>
     <h1>Covoiturage</h1>
